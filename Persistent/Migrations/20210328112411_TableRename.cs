b4038feb41c385d902ae8b77d2fe617fe7682c1a
Ahsan Rafiq <ahsanawan205@gmail.com>
2021-03-28 18:08:53 +0500

    Initial Commit

---
+++ Persistent/Migrations/20210328112411_TableRename.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Persistent.Migrations
+{
+    public partial class TableRename : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Actitivies",
+                table: "Actitivies");
+
+            migrationBuilder.RenameTable(
+                name: "Actitivies",
+                newName: "Activities");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Activities",
+                table: "Activities",
+                column: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Activities",
+                table: "Activities");
+
+            migrationBuilder.RenameTable(
+                name: "Activities",
+                newName: "Actitivies");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Actitivies",
+                table: "Actitivies",
+                column: "Id");
+        }
+    }
+}
